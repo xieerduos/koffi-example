9837c83784ab31dc6a92e20409767a3c9f9b8a0d
shaohai.li <1454598684@qq.com>
2024-09-07 16:03:29 +0800

    feat: koffi desktop user32 回调函数

--- desktop.js
+++ desktop.js
@@ -9,25 +9,25 @@ const HWND = koffi.alias("HWND", HANDLE);
 
 // 定义 EnumWindowsProc 回调函数的签名
 const EnumWindowsProc = koffi.proto(
-  "bool __stdcall EnumWindowsProc(HWND hwnd, long lParam)"
+  "bool __stdcall EnumWindowsProc(int32, int32)"
 );
 
 // 定义 EnumWindows 函数的签名，注意这里使用 koffi.pointer 传递函数指针
-const EnumWindows = user32.func(
-  "bool __stdcall EnumWindows(void* EnumWindowsProc, long)"
-);
+const EnumWindows = user32.func("__stdcall", "EnumWindows", "bool", [
+  koffi.pointer(EnumWindowsProc),
+  "int",
+]);
 
 // 枚举窗口的回调函数
-const myEnumWindowsProc = koffi.register(
-  (hwnd, lParam) => {
-    console.log("Found window handle:", hwnd);
-    // 返回 true 继续枚举，返回 false 停止枚举
-    return true;
-  },
-  EnumWindowsProc // 传递回调签名
-);
+let cb1 = koffi.register((hwnd, lParam) => {
+  console.log("[hwnd, lParam]", hwnd, lParam);
+  // 返回 true 继续枚举，返回 false 停止枚举
+  return true;
+}, koffi.pointer(EnumWindowsProc)); // 传递回调签名
 
 // 调用 EnumWindows 来列举所有顶级窗口，传递回调函数指针
-EnumWindows(myEnumWindowsProc, 0);
+EnumWindows(cb1, 0);
 
-console.log("Finished enumerating windows.");
+koffi.unregister(cb1);
+
+console.log("EnumWindowsProc", EnumWindowsProc);
